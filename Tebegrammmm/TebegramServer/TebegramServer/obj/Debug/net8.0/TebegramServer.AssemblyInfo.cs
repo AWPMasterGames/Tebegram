1cdb444ab360fbd2882a9440f151736c628b4a16
RespectAssasin <128396252+RespectAssasin@users.noreply.github.com>
2025-07-22 01:39:23 +0300

    +

--- Tebegrammmm/TebegramServer/TebegramServer/obj/Debug/net8.0/TebegramServer.AssemblyInfo.cs
+++ Tebegrammmm/TebegramServer/TebegramServer/obj/Debug/net8.0/TebegramServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TebegramServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35babbdbce92ce1ee1ac605813052085a709107e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c97061d617a44762b7fea9afe3d7e703d2553a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TebegramServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TebegramServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
