5fee27375af51adfbd730ce106ef22206f2dc8c0
RespectAssasin <128396252+RespectAssasin@users.noreply.github.com>
2025-12-26 20:21:29 +0300

    .
    .

--- TebegramClient/Tebegrammmm/Data/ServerData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tebegrammmm.Data
-{
-    public static class ServerData
-    {
-        private static string _ServerAdress = "http://localhost:5005";
-
-        public static string ServerAdress {  get { return _ServerAdress; } }
-    }
-}
