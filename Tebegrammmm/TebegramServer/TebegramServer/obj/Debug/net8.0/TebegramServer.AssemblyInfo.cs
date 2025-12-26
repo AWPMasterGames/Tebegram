2d86d3ac77eb4da301868b85ca24d12b4a244584
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2025-12-26 14:54:12 +0300

    update
    Change gitignor

--- Tebegrammmm/TebegramServer/TebegramServer/obj/Debug/net8.0/TebegramServer.AssemblyInfo.cs
+++ Tebegrammmm/TebegramServer/TebegramServer/obj/Debug/net8.0/TebegramServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TebegramServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be93121fba22e17e4c5c9d57a3f4ccf539379977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb7f9bf1664cf5aaccc29b271ebf38680c19f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TebegramServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TebegramServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
