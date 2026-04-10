b9c628ca257d5a64ea0a56c0c968722540c13c42
RespectAssasin <128396252+RespectAssasin@users.noreply.github.com>
2026-04-10 20:32:45 +0300

    Рефакторинг

---
+++ Tebegrammmm/Tebegrammmm/Classes/Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Tebegrammmm.Classes
+{
+    static class Log
+    {
+        private static string _CrashLogsDirectory;
+        private static object _lock = new object();
+        private static bool CheckDirectoryes()
+        {
+            _CrashLogsDirectory = $"{Directory.GetCurrentDirectory()}/CrashLogs";
+            if (!Directory.Exists(_CrashLogsDirectory))
+            {
+                Directory.CreateDirectory(_CrashLogsDirectory);
+            }
+            return true;
+        }
+
+        public static void Save(string log)
+        {
+            DateTime dateTime = DateTime.Now;
+            CheckDirectoryes();
+            lock (_lock)
+            {
+
+                if (!File.Exists($"{_CrashLogsDirectory}/{dateTime.ToString("dd.MM.yyyy")}.txt"))
+                {
+                    File.Create($"{_CrashLogsDirectory}/{dateTime.ToString("dd.MM.yyyy")}.txt").Close();
+                }
+                Thread.Sleep( 100 );
+                File.AppendAllText($"{_CrashLogsDirectory}/{dateTime.ToString("dd.MM.yyyy")}.txt", $"[{dateTime.ToString("dd.MM.yyyy HH:mm:ss")}]  {log}\n");
+            }
+        }
+    }
+}
---
+++ Tebegrammmm/Tebegrammmm/Data/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tebegrammmm.Data
+{
+    public static class UserData
+    {
+         public static User User { get; set; }
+    }
+}
