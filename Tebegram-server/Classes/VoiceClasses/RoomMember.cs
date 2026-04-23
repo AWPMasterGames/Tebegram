5a18d95da096bada8dc08ddac1867ef51e0ebcd0
Drunky <128396252+DrunkMan404@users.noreply.github.com>
2026-04-23 11:10:43 +0300

    +

---
+++ Tebegram-server/Classes/VoiceClasses/RoomMember.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace TebegramServer.Classes.VoiceClasses
+{
+    public class RoomMember
+    {
+        private User _User;
+        private WebSocket _Member;
+        public WebSocket Member { get { return _Member; } }
+        public User User { get { return _User; } }
+
+        public RoomMember(User user, WebSocket member)
+        {
+            _User = user;
+            _Member = member;
+        }
+
+        public async Task SendMe(byte[] message)
+        {
+            var bytes = message;
+
+            if (_Member.State == WebSocketState.Open)
+            {
+                var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+                await _Member.SendAsync(arraySegment, WebSocketMessageType.Binary, true, CancellationToken.None);
+            }
+        }
+        public async Task SendMeText(string text)
+        {
+
+            if (_Member.State == WebSocketState.Open)
+            {
+                var arraySegment = new ArraySegment<byte>(Encoding.UTF8.GetBytes(text));
+                await _Member.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+        public async Task Disconnect(WebSocketCloseStatus webSocketCloseStatus, string desciption,CancellationToken cancellationToken)
+        {
+            await Member.CloseAsync(webSocketCloseStatus, desciption, cancellationToken);
+            User.CallToken = "";
+        }
+    }
+}
